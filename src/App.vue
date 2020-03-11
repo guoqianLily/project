0b7bcc9c03b9d41ca33b4150ea8bb34df60c4627
guoqianLily <892952209@qq.com>
2020-03-11 12:36:45 +0800

    滚动条修改

--- src/App.vue
+++ src/App.vue
@@ -304,31 +304,22 @@ export default {
 
 
 <style lang="scss">
-//   ::-webkit-scrollbar {
-//     width: 8px;
-//     height: 4px;
-//     background-color: #fff;
-// }
-// ::-webkit-scrollbar-thumb {
-//     // border-radius:5px;
-//     -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, .3);
-//     background-color: rgba(0, 0, 0, .1)
-// }
 ::-webkit-scrollbar {/*滚动条整体样式*/
         width: 10px;     /*高宽分别对应横竖滚动条的尺寸*/
         height: 1px;
+        border-radius: 10px 10px 10px 10px ;
     }
 
 ::-webkit-scrollbar-thumb {/*滚动条里面小方块*/
-        border-radius: 10px;
+        border-radius: 10px 10px 10px 10px ;
         -webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);
-        background: #535353;
+        background: #c8c7c7;
 
     }
 
 ::-webkit-scrollbar-track {/*滚动条里面轨道*/
         -webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);
-        border-radius: 10px;
+      border-radius: 10px 10px 10px 10px ;
         background: #EDEDED;
 
 }
